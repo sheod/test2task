8afb56b9c30852f3411ec55204057ae4b28c486c
Sheod <Sheod!506>
2016-06-07 10:05:46 +0300

    setTimeout

--- js/app.js
+++ js/app.js
@@ -1,85 +1,82 @@
 'use strict';
-let app = new Object()
 
-app.__cache = {}
+var app = new Object();
 
-app.log = function(obj) {
-    console.log(obj)
-}
+app.__cache = {};
 
-app.addObject =  function(url, callbackFunc) {
-    let intervalId = setInterval(() => {
-        if(!this.__cache[url]) {
-            this.__cache[url] = url
-            let xhr = new XMLHttpRequest()
-            xhr.open('GET', url, true)
-            xhr.send()
+app.log = function (obj) {
+    console.log(obj);
+};
 
-            xhr.onreadystatechange = () => {
+app.addObject = function (url, callbackFunc) {
+    var _this = this;
 
-                if (xhr.readyState != 4) {
-                    return
-                }
-                delete this.__cache[url]
-                if (xhr.status != 200) {
-                    clearInterval(intervalId)
-                    return this.error(xhr);
-                }
+    var b = function b() {
+        if (!_this.__cache[url]) {
+            (function () {
+                _this.__cache[url] = url;
+                var xhr = new XMLHttpRequest();
+                xhr.open('GET', url, true);
+                xhr.send();
+                xhr.onreadystatechange = function () {
 
-                this.response = xhr.responseText
-                let elem = this.__createDOMObject(JSON.parse(this.response))
-                callbackFunc(elem)
-                clearInterval(intervalId)
-            }
+                    if (xhr.readyState != 4) {
+                        return;
+                    }
+                    delete _this.__cache[url];
+                    if (xhr.status != 200) {
+                        return _this.error(xhr);
+                    }
+
+                    _this.response = xhr.responseText;
+                    var elem = _this.__createDOMObject(JSON.parse(_this.response));
+                    callbackFunc(elem);
+                };
+            })();
+        } else {
+            setTimeout(b, 10);
         }
-    },10)
-}
+    };
+    setTimeout(b, 10);
+};
 
+app.error = function (status) {
+    console.log(status.status + ' - ' + status.statusText);
+};
 
+app.__getData = function (url) {};
 
-app.error = function(status) {
-    console.log(`${status.status} - ${status.statusText}`)
-}
+app.__createDOMObject = function (obj) {
+    var elem = document.createElement(obj.tag);
+    elem.textContent = obj.content;
 
-app.__getData = function(url) {
-
-}
-
-app.__createDOMObject = function(obj) {
-    let elem = document.createElement(obj.tag)
-    elem.textContent = obj.content
-
-    for (let attr in obj.attr) {
-        elem.setAttribute(attr, obj.attr[attr])
+    for (var attr in obj.attr) {
+        elem.setAttribute(attr, obj.attr[attr]);
     }
 
-    for (let event in obj.events) {
-        elem.addEventListener(event, new Function(obj.events[event]))
+    for (var event in obj.events) {
+        elem.addEventListener(event, new Function(obj.events[event]));
     }
 
-    for (let parameter in obj.style) {
-        elem.style[parameter] = obj.style[parameter]
+    for (var parameter in obj.style) {
+        elem.style[parameter] = obj.style[parameter];
     }
 
-    return elem
-}
+    return elem;
+};
 
+app.addObject('json/data4.json', function (obj) {
+    document.body.appendChild(obj);
+});
 
+app.addObject('json/data2.json', function (obj) {
+    document.body.appendChild(obj);
+});
 
+app.addObject('json/data.json', function (obj) {
+    document.body.appendChild(obj);
+});
 
-
-app.addObject('json/data4.json', function(obj) {
-    document.body.appendChild(obj)
-})
-
-app.addObject('json/data2.json', function(obj) {
-    document.body.appendChild(obj)
-})
-
-app.addObject('json/data.json', function(obj) {
-    document.body.appendChild(obj)
-})
-
-app.addObject('json/data.json', function(obj) {
-    document.body.appendChild(obj)
-})
+app.addObject('json/data.json', function (obj) {
+    document.body.appendChild(obj);
+});
